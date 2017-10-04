e39953a873b0e55868b1f5e7e26d9501d3a446bd
Pavel Rak <rak.pavel.89@gmail.com>
2017-10-04 16:05:15 +0200

    update withForm decorator

--- src/modules/forms/decorators/withForm.js
+++ src/modules/forms/decorators/withForm.js
@@ -1,118 +1,98 @@
 import React, { PropTypes as T } from 'react';
-import { isEmpty, reduce, constant } from 'lodash';
+import { isEmpty, reduce, isFunction, constant } from 'lodash';
 import dot from 'dot-object';
 import { reduxForm, stopSubmit } from 'redux-form';
-import { compose, withProps, branch } from 'recompose';
+import { compose, withProps, lifecycle, withHandlers } from 'recompose';
+import omitProps from 'utils/omitProps';
 import { connect } from 'react-redux';
 
 import validateByJsonSchema from '../validateByJsonSchema';
 import mergeWithArrays from '../mergeWithArrays';
 import assignDefaultsToRequiredObjectProperties from '../assignDefaultsToRequiredObjectProperties';
+import normalizeEmptyValues from '../normalizeEmptyValues';
 
-/**
- * Wraps component with redux-form enhanced with JSON schema validation
- *
- */
-export default function withForm({
-	schema = {},
-	errorMessagesPrefix,
-	errorMessages = {},
-	validate: userValidate,
-	initialValues,
-	...config
-} = {}) {
-	const validate = (values, props) => {
-		const {
-			schema: propsSchema,
-			errorMessagesPrefix: propsErrorMessagesPrefix,
-			errorMessages: propsErrorMessages,
-			userValidate: propsUserValidate,
-		} = props;
-		const finalUserValidate = propsUserValidate || userValidate;
-
-		const finalErrorMessagesPrefix = propsErrorMessagesPrefix || errorMessagesPrefix;
-		let finalErrorMessages = propsErrorMessages || errorMessages;
-		if (finalErrorMessagesPrefix) {
-			finalErrorMessages = dot.pick(finalErrorMessagesPrefix, dot.object({ ...finalErrorMessages }));
-		}
-		const finalSchema = propsSchema||schema;
-		const valuesToValidate = assignDefaultsToRequiredObjectProperties(values, finalSchema);
-		const validateJsonSchemaErrors = validateByJsonSchema(
-			valuesToValidate,
-			propsSchema || schema,
-			finalErrorMessages
-		);
-
-		const userValidateErrors = finalUserValidate ? finalUserValidate(values, props) : {};
-
-		const finalUserValidateErrors = dot.object(
-			reduce(
-				dot.dot({ ...userValidateErrors }),
-				(result, value, key) => {
-					const path = `${key}.${value}`;
-					result[key] = dot.pick(path, finalErrorMessages) || value; // eslint-disable-line no-param-reassign
-					return result;
+const withForm = (options = {}) => {
+	return compose(
+		withProps((props) => {
+			return isFunction(options) ? options(props) : options;
+		}),
+		connect(
+			false,
+			(dispatch) => ({
+				setExternalErrors: (targetForm, errors) => {
+					dispatch(stopSubmit(targetForm, errors));
 				},
-				{}
-			)
-		);
+			})
+		),
+		withHandlers({
+			validate: ({
+				schema,
+				errorMessagesPrefix,
+				errorMessages,
+				validate: userValidate,
+			}) => (values, props) => {
+				const {
+					schema: propsSchema,
+					errorMessagesPrefix: propsErrorMessagesPrefix,
+					errorMessages: propsErrorMessages,
+					userValidate: propsUserValidate,
+				} = props;
+				const finalUserValidate = propsUserValidate || userValidate;
 
-		return mergeWithArrays({}, validateJsonSchemaErrors, finalUserValidateErrors);
-	};
+				const finalErrorMessagesPrefix = propsErrorMessagesPrefix || errorMessagesPrefix;
+				let finalErrorMessages = propsErrorMessages || errorMessages;
+				if (finalErrorMessagesPrefix) {
+					finalErrorMessages = dot.pick(finalErrorMessagesPrefix, dot.object({ ...finalErrorMessages }));
+				}
+				const finalSchema = propsSchema||schema;
+				const valuesToValidate = normalizeEmptyValues(
+					assignDefaultsToRequiredObjectProperties(values, finalSchema),
+					finalSchema,
+				);
+				const validateJsonSchemaErrors = validateByJsonSchema(
+					valuesToValidate,
+					propsSchema || schema,
+					finalErrorMessages
+				);
 
-	return function wrapWithForm(WrappedComponent) {
-		class FormContainer extends React.Component {
+				const userValidateErrors = finalUserValidate ? finalUserValidate(values, props) : {};
 
-			static propTypes = {
-				form: T.string,
-				submitFailed: T.bool,
-				errors: T.object, // eslint-disable-line
-				setExternalErrors: T.func.isRequired,
-			};
+				const finalUserValidateErrors = dot.object(
+					reduce(
+						dot.dot({ ...userValidateErrors }),
+						(result, value, key) => {
+							const path = `${key}.${value}`;
+							result[key] = dot.pick(path, finalErrorMessages) || value; // eslint-disable-line no-param-reassign
+							return result;
+						},
+						{}
+					)
+				);
 
-			componentWillMount() {
-				const { errors: nextErrors } = this.props;
-				this.checkErrors({}, nextErrors, false);
-			}
-
-			componentWillReceiveProps(nextProps) {
-				const { errors: currentErrors } = this.props;
-				const { errors: nextErrors, submitFailed: nextSubmitFailed } = nextProps;
-				this.checkErrors(currentErrors, nextErrors, nextSubmitFailed);
-			}
-
-			checkErrors(currentErrors, nextErrors, nextSubmitFailed) {
+				return mergeWithArrays({}, validateJsonSchemaErrors, finalUserValidateErrors);
+			},
+			checkErrors: ({ setExternalErrors, form: formName }) => (currentErrors, nextErrors, nextSubmitFailed) => {
 				if (
 					!isEmpty(nextErrors) && !nextSubmitFailed && nextErrors !== currentErrors
 				) {
-					const { setExternalErrors, form: formName } = this.props;
 					setExternalErrors(formName, nextErrors);
 				}
 			}
-
-			render() {
-				return (
-					<WrappedComponent {...this.props} />
-				);
+		}),
+		lifecycle({
+			componentWillMount() {
+				const { errors: nextErrors, checkErrors } = this.props;
+				checkErrors({}, nextErrors, false);
+			},
+			componentWillReceiveProps(nextProps) {
+				const { errors: currentErrors, checkErrors } = this.props;
+				const { errors: nextErrors, submitFailed: nextSubmitFailed } = nextProps;
+				checkErrors(currentErrors, nextErrors, nextSubmitFailed);
 			}
-		}
-
-		return compose(
-			connect(
-				false,
-				(dispatch) => ({
-					setExternalErrors: (targetForm, errors) => {
-						dispatch(stopSubmit(targetForm, errors));
-					},
-				})
-			),
-			reduxForm(
-				{
-					...config,
-					validate,
-					initialValues,
-				}
-			),
-		)(FormContainer);
-	};
+		}),
+		omitProps(['schema', 'errorMessagesPrefix', 'errorMessages', 'userValidate', 'checkErrors']),
+		reduxForm(),
+	);
 }
+
+export default withForm;
